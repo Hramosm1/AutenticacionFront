1684e36b21c41595691bc6eaac8e39c56db30254
Diego Jared Donis Soto <diego.jdonis@gmail.com>
2022-05-15 14:48:15 -0600

    servicios backend e interfaces

---
+++ src/app/core/interfaces/backend.ts
@@ -0,0 +1,12 @@
+export interface Aplicaciones {
+  id: number
+  nombre: string
+}
+export interface Roles {
+}
+export interface Modulos {
+}
+export interface Permisos {
+}
+export interface PermisosEspeciales {
+}
