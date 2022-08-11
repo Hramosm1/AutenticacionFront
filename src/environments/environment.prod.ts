6c55d42c4f6d4f8fb48f822260307cc1b8fcef6b
Diego Jared Donis Soto <64775200+DiegoDoniss@users.noreply.github.com>
2022-08-11 09:15:23 -0600

    configuracion variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    firebase: {
+    /*firebase: {
         projectId: 'auth-1c7c7',
         appId: '1:480457543365:web:b02d6e6091cb7bc0da2aac',
         storageBucket: 'auth-1c7c7.appspot.com',
         apiKey: 'AIzaSyDP4dzTu5n59p2Ap0Kye61j992HjPm5-KU',
         authDomain: 'auth-1c7c7.firebaseapp.com',
         messagingSenderId: '480457543365',
-    },
+    },*/
     production: true,
-    serverUrl: 'http://192.168.8.28:9401/'
+    serverUrl: 'http://192.168.8.66:9401/'
 };
